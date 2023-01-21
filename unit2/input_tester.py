33feaea8d4b0e3253a79a73c060c5f170d2d8da2
rakaczma <rakaczma@student.wszib.edu.pl>
2023-01-21 09:29:51 +0100

    1

---
+++ unit2/input_tester.py
@@ -0,0 +1,25 @@
+# print("Jak masz na imię?")
+# name = input()
+# print("Witaj " + name + "! Jak leci mordo?")
+
+# number = int("2")
+# number = "2"
+
+# temp = float("36")
+# print(temp)
+
+# age = 24
+# print("Mam", age, "lata.")
+# print("Mam " + str(age) + "lata.")
+
+# string_number = input("Podaj liczbę całkowitą:")
+# multiplier = 9
+# result = multiplier * int(string_number)
+# print("Gdy liczbę " + string_number + " pomnożymy przez " + str(multiplier) + " to otrzymamy " + str(result))
+
+# a = float(input("Wprowadż długość pierwszego boku: "))
+# b = float(input("Wprowadż długość drugiego boku: "))
+# result = (a ** 2 + b ** 2) ** .5
+# print("Długość przeciwprostokątnej wynosi " + str(result))
+
+print("Elo mordo xd " * 8)
